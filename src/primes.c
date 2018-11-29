1199acd35890d2b103f7120826ba940d07a851ca
Alice Jacka <jacka.alice@gmail.com>
2018-11-29 17:14:45 +1100

    moved from llu to _uint64_t and some other stuff

--- src/primes.c
+++ src/primes.c
@@ -3,120 +3,107 @@
 #include <math.h>
 
 // set the chunk size for each thread to calculate
-const unsigned long long chunkSize = 10000;
+const __uint64_t chunkSize = 10000;
 
 char threadCount = 2;
 
 // create shared variables for threads
-unsigned long long input     = 0;
-unsigned long long nextChunk = 3;
-
-char lastChunk = 0; // is it the last chunk?
-char prime     = 0; // is it a prime? (0=yes 1=no)
+__uint64_t potentialPrime = 0;
+__uint64_t nextChunkStart = 3;
+__uint64_t maxCompare     = 0;
+char       couldBePrime   = 1;
+char       keepGoing      = 1;
 
 // create the mutex
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 // do a chunk of comparasons
-char checkChunk(unsigned long long input, unsigned long long start) {
-    const unsigned long long end = start + chunkSize;
+char checkChunk(__uint64_t input, __uint64_t start) {
+    const __uint64_t end = start + chunkSize;
 
-    for (unsigned long long compare = start; compare <= end; compare += 2) {
+    for (__uint64_t compare = start; compare <= end; compare += 2) {
         if (input % compare == 0) {
-            return 1;
+            printf("%lu is devisable by %lu\n", );
+            return 0;
         }
     }
 
-    return 0;
+    return 1;
 }
 
 void *thread() {
-    unsigned long long chunkStart = 3;
+    __uint64_t chunkStart = 3;
 
-    while(1) {
-        // if it's not a prime
-        if (prime) {
-            break;
-        }
-
-        // ask for chunk
+    while (keepGoing) {
         pthread_mutex_lock(&mutex); // lock
 
-        chunkStart = nextChunk;
+        chunkStart      = nextChunkStart;
+        nextChunkStart += chunkSize;
 
-        // if this is the last chunk for this input
-        if (nextChunk + chunkSize >= input / 5) {
-            lastChunk = 1; // save that fact
-        } else {
-            nextChunk += chunkSize;
+        // if we're on the last chunk
+        if (chunkStart + chunkSize >= maxCompare) {
+            keepGoing = 0; // tell other threads to stop at the end of their chunk
         }
 
-        pthread_mutex_unlock(&mutex); // unlock
+        pthread_mutex_unlock(&mutex);
 
 
-        // if it's not prime
-        if (checkChunk(input, chunkStart)) {
+        if (!checkChunk(potentialPrime, chunkStart)) {
             pthread_mutex_lock(&mutex); // lock
-            prime = 1;
-            pthread_mutex_unlock(&mutex); // unlock
-            break;
-        } else {
-            // so far it's a prime, if it's also the last chunk
-            if (lastChunk) {
-                break;
-            }
+            keepGoing    = 0;  // tell other threads to stop at the end of their chunk
+            couldBePrime = 0;
         }
     }
 
     return NULL;
 }
 
-// determine if a number is a prime or not
-char isPrime(unsigned long long inputToTest) {
-    input = inputToTest;
-    unsigned long long maxCompare = input / 5;
-    int rc[16];
+char isPrime(__uint64_t input) {
+    maxCompare = input / 3;
     pthread_t threads[16];
+    int threadError;
 
     if (input % 2 == 0) {
-        return 1;
+        return 0;
     }
 
-    if (input % (input / 3) == 0) {
-        return 1;
-    }
+    couldBePrime = 1;
 
-    for(char i = 0; i < threadCount; i++)
+    for (char i = 0; i < threadCount; i++)
     {
-        if( (rc[i]=pthread_create( &threads[i], NULL, &thread, NULL)) )
+        if( (threadError=pthread_create( &threads[i], NULL, &thread, NULL)) )
         {
-            printf("Thread creation failed: %d\n", rc[i]);
+            printf("Thread creation failed: %d\n", threadError);
         }
     }
 
-    for(char i = 0; i < threadCount; i++)
+    for (char i = 0; i < threadCount; i++)
     {
         pthread_join(threads[i], NULL);
     }
 
-    return 0;
+    if (couldBePrime) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 int main(int argc, char const *argv[])
 {
     if (1) {
-        unsigned long long input = 50000000021; // 50000000021 10000000019 5000000029 383
+        __uint64_t input = 50000000021; // 50000000021 10000000019 5000000029 383
 
-        if (isPrime(input) == 0) {
+        if (isPrime(input) == 1) {
             printf("Yeah\n");
         } else {
             printf("Nah\n");
         }
     } else {
-        for (unsigned long long input = 50000000000; input < 100000000000; input++)
+        for (__uint64_t input = 0; input < 100; input++)
         {
             if (isPrime(input) == 1) {
-                printf("%llu\n", input);
+                printf("%lu\n", input);
             }
         }
     }
